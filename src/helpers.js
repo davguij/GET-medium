bebca0ef47282f5f556298eb6958285429eb9d51
David Guijarro <guijarro.dav@gmail.com>
2017-12-16 19:36:25 +0100

    chore: CI and coverage report

--- src/helpers.js
+++ src/helpers.js
@@ -1,10 +1,14 @@
 const http = require('axios');
 
 const mediumFetcher = async username => {
-  const req = await http.get(`https://medium.com/@${username}/latest`, {});
-  const posts = req.data;
-  const json = JSON.parse(posts.replace('])}while(1);</x>', ''));
-  return json.payload;
+  try {
+    const req = await http.get(`https://medium.com/@${username}/latest`, {});
+    const posts = req.data;
+    const json = JSON.parse(posts.replace('])}while(1);</x>', ''));
+    return json.payload;
+  } catch (err) {
+    return err;
+  }
 };
 
 module.exports = { mediumFetcher };
